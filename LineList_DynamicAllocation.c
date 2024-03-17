9ec5647ef15d9f033ceb37ab230ecf8c3fd8f023
qinglan-qaq <1710202731@qq.com>
2024-03-17 10:30:05 +0800

    链表动态分配的初始化建立

--- LineList_DynamicAllocation.c
+++ LineList_DynamicAllocation.c
@@ -4,7 +4,8 @@
 
 #include "stdio.h"
 #include "stdlib.h"
-#define MaxSize 20
+
+#define InitSize 20
 #define True 1
 #define False 0
 
@@ -15,3 +16,67 @@ typedef struct {
     int Maxsize;
     int length;
 } SqList;
+
+//初始化
+Status InitList(SqList *L) {
+//    申请连续的内存空间
+    L->data = (int *) malloc(InitSize * sizeof(int));
+//如果这个空间为空
+    if (L->data == NULL) {
+        printf("LineList is empty!");
+        return False;
+    }
+
+    L->length = 0;
+    L->Maxsize = InitSize;
+    printf("Init Success!");
+    return True;
+}
+
+//判断是否为空
+Status Empty(SqList L) {
+    if (L.length == 0){
+        printf("LineList is Empty!");
+        return True;
+    }
+}
+
+//判满
+Status Full(SqList L) {
+    if (L.length >= L.Maxsize){
+        printf("LineList is Full!");
+        return True;
+    }
+}
+
+//插入
+Status InsertList(SqList *L, int location, int value) {
+
+}
+
+//删除
+
+//按位查找
+
+//按值查找
+
+//动态扩展
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
